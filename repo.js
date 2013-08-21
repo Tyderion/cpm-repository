21b6ab28e0dbc1e956e7172e64cba160212b8ac3
Gabriel Nadler <gabriel.nadler@swisscom.com>
2013-08-21 07:43:47 +0200

    Added my programs to be auto-loaded into the redis-store

--- repo.js
+++ repo.js
@@ -3,38 +3,59 @@ redis = require("redis");
 
 app = express();
 
+
+store_prefix = "programs:"
+listname = "programlist"
+client = redis.createClient()
+
+
+// Add my programs to the store
+saveNewProgram("cpm", "wP9VAie2")
+saveNewProgram("master", "AKJKmswF")
+saveNewProgram("slave", "5KQuXrKU")
+saveNewProgram("relay", "QEvpL30r")
+saveNewProgram("client", "qUW9dPfG")
+saveNewProgram("order", "b4E68ZN5")
+saveNewProgram("tunnel", "MjqSE2RC")
+saveNewProgram("rect", "MzRUZcw6")
+saveNewProgram("archie", "H19M3mwm") // API
+
+// client.set("blabla", "ABCDEF")
+
 app.configure(function(){
     app.use(express.bodyParser());
     app.use(app.router);
     app.use(express.logger());
 });
 
-function getCode (name) {
-  return "ACode"+Math.random();
-}
 
 app.param('name', function(req,res, next, name){
-  //do something with id
-  //store id or other info in req object
-  //call next when done
-  req.info = { name: name, code: getCode(name) }
-  next();
+  client.get(store_prefix+name, function (error, code) {
+    console.log("got code: " + code)
+    if (code !== null) {
+      res.send({ name: name, code: code})
+    } else {
+      next("Code does not exist for " + name)
+    }
+      });
 });
 
 app.get("/test", function (req, res) {
   res.send("Hello World")
 });
 
-app.get("/api/v1/getCode/:name", function (req, res) {
-  res.send(req.info)
-});
+app.get("/api/v1/getCode/:name");
 
 app.get("/api/v1/list", function (req, res) {
-  res.send(["cpm", "master", "slave"]);
+  client.smembers(listname, function (error, result) {
+      res.send(result)
+  })
 });
 
 function saveNewProgram (name, code) {
-  // body...
+  console.log("Saving " + code + " as " + name)
+  client.sadd(listname, name)
+  client.set(store_prefix+name, code)
 }
 
 app.post("/api/v1/publish", function(req, res) {
@@ -43,7 +64,7 @@ app.post("/api/v1/publish", function(req, res) {
   if (typeof name !== "undefined" && name !== null) {
     if (typeof code !== "undefined" && code !== null) {
       saveNewProgram(name, code)
-      res.send("Saved")
+      res.send("Saved " + name)
     }
   }
   res.send("Need name and code to save the program")
